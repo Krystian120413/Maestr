e762620f7147ce8d9920f376c26c16c7d398ee51
Krystian120413 <Krystian12053@gmail.com>
2021-06-10 22:37:30 +0200

    THE END

--- login.php
+++ login.php
@@ -17,7 +17,7 @@
 
     if($value >= 0){
         // zapis
-        $ciastka = Array('email' => $email);
+        $ciastka = Array('email' => $email, 'passwd' => $passwd);
         setcookie('ciastka', serialize($ciastka), time()+3600);
 
         // odczyt zabezpieczony przed nieistniejącym ciasteczkiem
